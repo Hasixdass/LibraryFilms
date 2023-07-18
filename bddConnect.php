ff12807a951339bd5ac3e3ad619b2384f2f17034
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-18 19:37:08 +0300

    Update bddConnect.php

--- bddConnect.php
+++ bddConnect.php
@@ -1,7 +1,10 @@
 <?php
+	// Informations de connexion à la base de données
 	$login="root";
 	$server="localhost";
 	$pass="";
 	$bdname="libraryfilms";
+	
+	//connecter  à la base de données
 	$con=mysqli_connect($server, $login, $pass, $bdname);
 ?>
