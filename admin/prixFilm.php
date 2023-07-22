4c69c5a2495376dbcbfd75acc7f36ec61dbf7472
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-22 08:42:34 +0300

    Update prixFilm.php

--- admin/prixFilm.php
+++ admin/prixFilm.php
@@ -1,57 +1,33 @@
 <HTML>
-	<HEAD> <TITLE> TABLEAU DES PRIX</TITLE> </HEAD>
-	<style>
-			table {
-				border-collapse: collapse;
-				width: 75%;
-			}
-			
-			th, td {
-				padding: 8px;
-				text-align: left;
-				border-bottom: 1px solid #ddd;
-			}
-			
-			tr:hover {
-				background-color: #f5f5f5;
-			}
-			
-			h2 {
-				color: #333;
-			}
-			
-			form {
-				display: inline;
-			}
-			
-			#btt {
-				padding: 5px 10px;
-				background-color: #4CAF50;
-				color: white;
-				border: none;
-				border-radius: 4px;
-				cursor: pointer;
-			}
-			
-			button:hover {
-				background-color: #45a049;
-			}
-		</style>
+	<HEAD> 
+		<TITLE> TABLEAU DES PRIX</TITLE>
+		<link rel="stylesheet" type="text/css" href="../style/prixFilm.css">
+	</HEAD>
+	
 	<BODY>
 		<?php
-		 include "../bddConnect.php";
 		 // connexion à la base de données
+		 include "../bddConnect.php";
 		 if(!$con)
 		 {
+			// Vérifier si la connexion a échoué et afficher un message d'erreur en cas d'échec
 			die("La connexion à la base de données a échoué:" .mysqli_connect_error());
 		 }
 		 else
 		 {
+			// Si la connexion est réussie, afficher un titre pour la section de prix des films
 			echo "<h2>Le prix du films: </h2> <BR>";
-			$sql="  SELECT genre.id_genre ,nom_genre, prix FROM genre  JOIN prix  ON genre.id_genre = prix.id_genre";
+			
+			// Requête pour récupérer les données des genres et leurs prix associés depuis les tables "genre" et "prix"
+			$sql="  SELECT genre.id_genre ,nom_genre, prix 
+					FROM genre  JOIN prix
+					ON genre.id_genre = prix.id_genre 
+					ORDER by nom_genre";
+			
+			// Exécute la requête
 			$res=mysqli_query($con, $sql);
 			
-			// Affichage des données
+			// Affichage des données dans un tableau
 			echo '<TABLE border=2 >';
 				echo"<TR>";
 					echo "<TH> Genre </TH>";
@@ -65,7 +41,7 @@
 						
 						echo "<TD>" .$ligne["nom_genre"]."</TD>";
 						echo "<TD>" .$ligne["prix"]."</TD>";
-						echo '<TD>
+						echo '<TD class="btt">
 								<FORM method="POST" action="RetourModification.php">
 									<input type="hidden" name="ID" value= "'.$ligne["id_genre"].'">
 									<input type="hidden" name="GG" value= "'.$ligne["nom_genre"].'">
@@ -73,9 +49,9 @@
 									<button type="submit"> Modifier </button>
 								</FORM>
 							  </TD>';
-						echo '<TD>
+						echo '<TD class="btt">
 								<FORM method="POST" action="suppression.php">
-									<button type="submit" id="btt" name="id" value="'.$ligne["id_genre"].'">Supprimer</button>
+									<button type="submit" name="id" value="'.$ligne["id_genre"].'">Supprimer</button>
 								</FORM>
 							  </TD>';
 					echo "</TR>";
