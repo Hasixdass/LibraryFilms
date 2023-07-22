33801555eefefd51e74f4878c773e0022af86a35
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-22 08:44:21 +0300

    Update suppression.php

--- admin/suppression.php
+++ admin/suppression.php
@@ -1,18 +1,35 @@
 <HTML>
-	<HEAD> <TITLE> SUPPRESSION </TITLE> </HEAD>
+	<HEAD> <TITLE> SUPPRESSION GENRE </TITLE> </HEAD>
 	</BODY>
 		<?php
+		 // Récupérer l'ID du genre à supprimer depuis le formulaire dans la page prixFilm.php
 		 $id=$_POST['id'];
+		 
+		 //connexion à la base de données
 		 include "../bddConnect.php";
+		 
+		 // Requête pour supprimer le genre et ses prix associés de la base de données
 		 $sql="DELETE genre, prix
 			   FROM genre
 			   INNER JOIN prix ON genre.id_genre = prix.id_genre
 			   WHERE genre.id_genre ='$id'";
-		 mysqli_query($con, $sql);
-		 mysqli_close($con);
-		 mysqli_query($con, $sql);
-		 header("Location: acceuilAdmin.php");
-		 // fermer la connexion
+			   
+		// Exécuter la requête
+		// Vérifier si la requête a été exécutée avec succès
+		if (mysqli_query($con, $sql)) 
+		{
+			// La suppression a été effectuée avec succès, rediriger vers la page de confirmation de suppression
+			header("Location: RetourOk.php");
+			exit();
+		} 
+		else 
+		{
+			// La suppression a échoué, rediriger vers la page d'erreur
+			header("Location: erreurSuppression.php");
+			exit();
+		}
+		 
+		 // Fermer la connexion à la base de données
 		 mysqli_close($con);
 		?>
 	</BODY>
