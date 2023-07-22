689f1c15790a4bda2117a638f8fda2e4fd0f0ebb
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-22 08:35:14 +0300

    Update ajoutFilm.php

--- admin/ajoutFilm.php
+++ admin/ajoutFilm.php
@@ -2,51 +2,7 @@
 <html>
 	<head>
 		<title>Formulaire d'ajout de film</title>
-		<style>
-			body
-			{
-				background-color: rgba(224, 199, 178, 0.2);
-			}
-
-			#formulaire {
-				width: 550px;
-				margin: 0 auto;
-				background-color: rgba(0.6);
-				padding: 100px;
-				border-radius: 10px;
-				box-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
-			}
-
-			label {
-				display: block;
-				margin-bottom: 10px;
-				font-weight: bold;
-			}
-
-			input[type="text"],
-			input[type="date"],
-			input[type="file"] {
-				width: 100%;
-				padding: 5px;
-				border: 1px solid #ccc;
-				border-radius: 4px;
-				box-sizing: border-box;
-				margin-bottom: 10px;
-			}
-
-			#submit {
-				background-color: #4CAF50;
-				color: white;
-				padding: 10px 20px;
-				border: none;
-				border-radius: 4px;
-				cursor: pointer;
-			}
-
-			#submit:hover {
-				background-color: #45a049;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="../style/ajoutFilm.css">
 	</head>
 	<body>
 	<form action="enregistrement.php" id="formulaire" method="POST" enctype="multipart/form-data">
@@ -59,8 +15,10 @@
 			// Connexion à la base de données
 			include "../bddConnect.php";
 
-			// Requête pour récupérer les genres
+			// Requête pour récupérer les genres du film
 			$sql = "SELECT id_genre, nom_genre FROM genre";
+			
+			// Exécute les requêtes
 			$res = mysqli_query($con, $sql);
 
 			// Boucle pour générer les options
@@ -74,14 +32,14 @@
 			?>
 		</select><br><br>
 
-		<label for="date_sortie">Date de sortie :</label>
+		<label >Date de sortie :</label>
 		<input type="date" id="date_sortie" name="date_sortie" required><br><br>
 
-		<label for="input1">Image :</label>
-		<input type="file" id="input1" name="image" required><br><br>
-
-		<label for="input2">Description :</label>
-		<input type="file" id="input2" name="fichier" required><br><br>
+		<label >Lien Image :</label>
+		<input type="text" id="input1" name="lienImage" required><br><br>
+		
+		<label >Description du film (fichier du type .txt) :</label>
+		<input type="file" name="fichier_txt" required>
 
 		<input type="submit" id="submit" name="submit" value="Ajouter">
 	</form>
