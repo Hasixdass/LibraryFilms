2890c586406878f8e7fe8173653db29481eb2bf3
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-22 08:37:14 +0300

    Update bbFilm.php

--- admin/bbFilm.php
+++ admin/bbFilm.php
@@ -1,204 +1,157 @@
 <html>
 	<head>
 		<title>Acceuil</title>
-		<style>
-			body
-			{
-				background: url('../image/hogwartsi.jpg') ;
-				background-repeat: no-repeat;
-				background-size: cover;
-				
-			}
-			header
-			{
-				background-color: 
-				border-radius: 4px;
-				display: flex;
-				align-items: center;
-				
-			}
-			.recherche input[type="submit"]
-			{
-				padding: 20px 1px;
-				background-color: #4caf50;
-				color: #ffffff;
-				border: none;
-				border-radius: 4px;
-				cursor: pointer;
-			}
-			.recherche input[type="text"]
-			{
-				padding: 20px 10px;
-				border: none;
-				border-radius: 4px;
-			}
-			.recherche
-			{
-				margin: auto;
-				display: flex;
-				align-items: center;
-				text-align: center;
-			}
-			.principal
-			{
-				margin: 30px auto;
-				width: 1200px;
-				padding: 40px;
-				background-color: rgba(255, 255, 255, 0.3);
-				box-shadow: 0px 0px 10px #888;
-				border-radius: 5px;
-			}
-			.solde
-			{
-				background-color: skyblue;
-				margin: auto;
-				align-items: center;
-				padding: 50px;
-			}
-			.image
-			{
-				display: inline-block;
-				margin-right: 10px;
-				overflow-x: auto;
-				display: flex;
-			}
-			div img
-			{
-				padding: 10px;
-			}
-			aside
-			{
-				max-width: 200px;
-				background-color: rgba(255, 2, 255, 0.8);
-			}
-			section
-			{
-				display: flex;
-				flex-wrap: wrap;
-			}
-			section > aside, section > article
-			{
-				flex: 1;
-				margin: 10px;
-				background-color: rgba(255, 255, 255, 0.5);
-				padding: 20px;
-				box-sizing: border-box;
-				border-radius: 5px;
-			}
-			h3
-			{
-				text-align: center;
-			}
-			table 
-			{
-				width: 100%;
-				border-collapse: collapse;
-				margin-top: 10px;
-			}
-			
-			th, td 
-			{
-				padding: 8px;
-				border-bottom: 1px solid #ddd;
-			}
-			#prix , #prix2
-			{
-				text-align: center;
-			}
-			th 
-			{
-				background-color: #f2f2f2;
-				font-weight: normal;
-			}
-			footer
-			{
-				background-color: #f9f9f9;
-				padding: 10px;
-				text-align: center;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="../style/acceuil.css">
 	</head>
 	<body>
 		<?php
 			session_start();
-		if (!isset($_SESSION['username'])) 
-		{
-			header("Location: ../index.html");
-			exit;
-		}
+			if (!isset($_SESSION["username"])) 
+			{
+				header("Location: index.html");
+				exit();
+			}
+			if ($_SERVER["REQUEST_METHOD"] == "POST") 
+			{
+				unset($_SESSION["username"]);
+				header("Location: index.html");
+				exit();
+			}
 			include "../bddConnect.php";
-			$sql2 = "SELECT nom_genre FROM genre ORDER  by nom_genre";
-			$res2=mysqli_query($con, $sql2);
-			echo '<div class="principal">';
-				echo '<header>';
-					echo '<a href="bbFilm.php"> <img src="../image/BbFilm-20-06-2023.png"></a>';
-					echo '<nav class="recherche">';
-						echo '<form method="POST" action="#">';
-							echo '<input type="text" placeholder="Recherche">';
-							echo '<input type="image" src="../image/icontexto-search-01_icon-icons.com_76897.png" width="40" height="40" alt="Submit">';
-						echo '</form>';
-					echo '</nav>';
-					echo '<div class="solde">';
-						echo '<h2><a href="acceuilAdmin.php">ADMIN</a></h2><br>';
-					echo '</div>';
-				echo '</header>';
-				echo '<nav class="pic">
-					<div class="image">
-						<a href="#">
-							<img src="../image/tyler-rake-2.jpg"  width="210" height="318">
-						</a>
-						<a href="#">
-							<img src="../image/fast-X.jpg"  width="210" height="318">
-						</a>
-						<a href="#">
-							<img src="../image/the-flash-4dx-poster.jpg"  width="210" height="318">
-						</a>
-						<a href="#">
-							<img src="../image/john.jpg"  width="210" height="318">
-						</a>
-						<a href="#">
-							<img src="../image/Black-Adam.jpg"  width="210" height="318">
-						</a>
-						<a href="#">
-							<img src="../image/avatar-the-way-of-water-poster.jpg"  width="210" height="318">
-						</a>    
-					</div>
-				</nav>';
-				echo '<section>
-					<aside>
-						<h3>Genre</h3>';
-						while($ligne2=mysqli_fetch_assoc($res2))
-						{
-							echo '<ul><li> <a href="#">' . $ligne2["nom_genre"] .'</a> </li></ul>';
-						}
-					echo'</aside>
-					<article>
-						<h2>FILM</h2>
-						<table border="1">
-							<thead>
-								<tr>
-									<th>NOM</th>
-									<th>PRIX</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>SISSUE</td>
-									<td id="prix">1000</td>
-								</tr>
-								<tr>
-									<td>WICK 4</td>
-									<td id="prix2">1000</td>
-								</tr>
-							</tbody>
-						</table>    
-					</article>
-				</section>';
-				echo'<footer>
-					<p>contact Admin : rafidyhasina@gmail.com</p>
-				</footer>';
-			echo "</div>";
-			mysqli_close($con);
+			$sql = "SELECT username, password, solde FROM login WHERE username='".$_SESSION["username"]."'";
+			$sql2 = "SELECT id_genre, nom_genre FROM genre ORDER BY nom_genre";
+			$res = mysqli_query($con, $sql);
+			$res2 = mysqli_query($con, $sql2);
+			$ligne = mysqli_fetch_array($res);
 		?>
+		<div class="principal">
+			<header>
+				<a href="bbFilm.php"> <img src="../image/BbFilm-20-06-2023.png"></a>
+				<nav class="recherche">
+					<form method="GET" action="../genre/recherche.php">
+						<input type="text" name="recherche" placeholder="Recherche">
+						<input type="image" src="image/icontexto-search-01_icon-icons.com_76897.png" width="40" height="40" alt="Submit">
+					</form>
+				</nav>
+				<div class="solde">
+					<a href="acceuilAdmin.php"><h2>ADMIN</h2></a>
+				</div>
+			</header>
+			<nav class="pic">
+				<div class="image">
+					<a href="../pageAchat.php?IDfilm=45">
+						<img src="https://zimage.cc/uploads/image/bca8060264e86148a7f5ab62212ea1f723f4a8a0.webp"  width="210" height="318">
+					</a>
+					<a href="../pageAchat.php?IDfilm=48">
+						<img src="https://zimage.cc/uploads/image/b8b9ff04c40b230bf28e8ad54961f79a0a6f71ba.webp"  width="210" height="318">
+					</a>
+					<a href="../pageAchat.php?IDfilm=44">
+						<img src="https://zimage.cc/uploads/image/46d325caf0458e37aac17b5441b6ba6c51b4b7cc.webp"  width="210" height="318">
+					</a>
+					<a href="../pageAchat.php?IDfilm=47">
+						<img src="https://zimage.cc/uploads/image/d8acd55cfb12dec9e30ef063df5472d6ab1003a3.webp"  width="210" height="318">
+					</a>
+					<a href="../pageAchat.php?IDfilm=42">
+						<img src="https://zimage.cc/uploads/image/9a69b43f9e5233008fd016ba3cc1da2d1af09207.jpg"  width="210" height="318">
+					</a>
+					<a href="../pageAchat.php?IDfilm=41">
+						<img src="https://zimage.cc/uploads/image/f5bb4150442c211c463d86a36cd8a86d3503255c.jpg"  width="210" height="318">
+					</a>    
+				</div>
+			</nav>
+			<section>
+				<aside>
+					<h3>Genre</h3>
+					<?php
+						$liens = array(
+						39 => "../genre/action.php",
+						40 => "../genre/aventure.php",
+						41 => "../genre/comedie.php",
+						42 => "../genre/drame.php",
+						44 => "../genre/espionnage.php",
+						43 => "../genre/famille.php",
+						45 => "../genre/fantastique.php",
+						46 => "../genre/horreur.php",
+						47 => "../genre/policier.php",
+						48 => "../genre/fiction.php"
+						);
+
+					while ($ligne2 = mysqli_fetch_assoc($res2)) 
+					{
+						$genreId = $ligne2["id_genre"];
+						$genre = $ligne2["nom_genre"];
+						
+						if (isset($liens[$genreId])) 
+						{
+							echo '<ul><li><a href="' . $liens[$genreId] . '">' . $genre . '</a></li></ul>';
+						} 
+					}
+					?>
+				</aside>
+				<article>
+					<h2>FILM</h2>
+					<table border=1>
+						<tr>
+							<th>Nom du film</th>
+							<th>Prix</th>
+						</tr>
+						<?php
+						$elements_par_page = 10;
+
+						if (isset($_GET['page']))
+						{
+							$page_en_cours = $_GET['page'];
+						}
+						else 
+						{
+							$page_en_cours = 1;
+						}
+
+						$premier_element = ($page_en_cours - 1) * $elements_par_page;
+						$req="SELECT film.id_images, film.nom_film, prix.prix
+							FROM film
+							JOIN genre ON film.id_genre = genre.id_genre
+							JOIN prix ON film.id_prix = prix.id_prix 
+							ORDER by date_sortie DESC 
+							LIMIT $premier_element, $elements_par_page ";
+						$resultat = mysqli_query($con, $req );
+
+						while ($ligne3 = mysqli_fetch_array($resultat)) 
+						{
+							echo "<tr>";
+							echo "<td><a href=\"../pageAchat.php?IDfilm=" . $ligne3['id_images'] . "\">" . $ligne3['nom_film'] . "</a></td>";
+							echo "<td>".$ligne3['prix']."</td>";
+							echo "</tr>";
+						}
+						$count="SELECT COUNT(*) AS total FROM film";
+						$resultat_total = mysqli_query($con, $count);
+						$ligne3_total = mysqli_fetch_array($resultat_total);
+						$total_elements = $ligne3_total['total'];
+
+						$nombre_de_pages = ceil($total_elements / $elements_par_page);
+
+						echo "<div>";
+						for ($i = 1; $i <= $nombre_de_pages; $i++) 
+						{
+							if ($i == $page_en_cours) 
+							{
+								echo "<span>$i</span>&nbsp;";
+							} 
+							else 
+							{
+								echo "<a href=\"".$_SERVER['PHP_SELF']."?page=$i\">$i</a>&nbsp;";
+							}
+						}
+						echo "</div>";
+
+						mysqli_close($con);
+						?>
+					</table>  
+				</article>
+			</section>
+			<footer>
+				<p>contact Admin: rafidyhasina@gmail.com</p>
+			</footer>
+		</div>
 	</body>
 </html>
