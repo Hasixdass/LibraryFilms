9d008eca42b83d9d5f136320de32931c6ad42515
Hasixdass <131137910+Hasixdass@users.noreply.github.com>
2023-07-22 08:32:41 +0300

    Update UpdateGenreEtPrix.php

--- admin/UpdateGenreEtPrix.php
+++ admin/UpdateGenreEtPrix.php
@@ -2,25 +2,34 @@
 	<HEAD> <TITLE> UPDATE </TITLE> </HEAD>
 	</BODY>
 		<?php
-		 $ID=$_POST['ID'];
-		 $genre=$_POST['genre'];
-		 $prix=$_POST['prix'];
+		 // Récupérer les données du formulaire de modification
+		 $ID=$_POST['ID']; // ID du genre à modifier
+		 $genre=$_POST['genre']; // Nouveau nom du genre
+		 $prix=$_POST['prix']; // Nouveau prix associé au genre
+		 
+		 // Inclure le fichier de connexion à la base de données
 		 include "../bddConnect.php";
 		 
-		 // Vérification de la connexion
+		 // Vérification de la connexion à la base de données
 		 if(!$con)
 		 {
 			die("La connexion à la base de données a échoué:" .mysqli_connect_error());
 		 }
 		 else
 		 {
+			// Requête pour mettre à jour le nom du genre et son prix dans les tables genre et prix
 			$sql= "UPDATE genre
 				   JOIN prix ON genre.id_genre = prix.id_genre
 				   SET genre.nom_genre = '$genre', prix.prix = '$prix'
                    WHERE genre.id_genre='$ID' AND prix.id_genre='$ID' ";
+				   
+			// Exécuter la requête 
 			mysqli_query($con, $sql);
-			header("Location: acceuilAdmin.php");
+			
+			// Rediriger vers la page de confirmation de modification
+			header("Location: RetourOk.php");
 		 }
+		 // Fermer la connexion à la base de données
 		 mysqli_close($con);
 		?>
 	</BODY>
